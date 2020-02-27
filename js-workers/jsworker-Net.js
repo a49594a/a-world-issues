dc14ceed57f6b63f6f4874f605d9c55ccca69df3
阳天极苍 <2388621687@qq.com>
2020-02-27 16:56:39 +0800

    Update JSWorker-Net

--- js-workers/jsworker-Net.js
+++ js-workers/jsworker-Net.js
@@ -36,27 +36,43 @@ function main(args){
                 this.hiddenValue = initArray(this.hidden);
                 this.outputValue = initArray(this.output);
                 this.outputValueExpect = args.outputValueExpect;
-                this.action = "train";
                 if(!this.outputValueExpect.length||!this.weight1.length||!this.weight2.length
                     ||!this.inputValue.length)return {"error":"Some arrays are empty."}
                     /*V1: Fixed here*/
                 forward(this);
                 backward(this);
                 var config = {
-                    action:this.action,
                     outputValue:this,outputValue,
                     error:""
                 }
                 return config;
             case "speed":
                 this.speed = args.speed;
-                this.action = "train";
                 var config = {
-                    action:this.action,
                     outputValue:this,outputValue,
                     error:""
                 }
                 return config;
+            case "forward":
+            	this.inputValue = args.inputValue;
+            	this.hiddenValue = initArray(this.hidden);
+            	this.outputValue = initArray(this,output);
+            	if(!this.inputValue.length)return {"error":"Array inputValue is empty."}
+            	forward(this);
+            	var config = {
+            		outputValue:this.outputValue,
+            		error:""
+            	}
+            	return config;
+            case "backward":
+            	this.outputValueExpect = args.outputValueExpect;
+            	backward(this);
+            	var config = {
+            		error:""
+            	}
+            	return config;
+            case "info":
+            	return this;
         }
 
     }catch(err){
@@ -176,7 +192,7 @@ function arrayDot(array1,array2){
 //    var network = main({"input":"4","output":"4","speed":"1","hidden":"8","action":"init","outputValueExpect":[1,0,0,0.5],"inputValue":[0,0,0,0],"weight1":[[0.40354305786332345,0.43711884944299584,0.09179569773269325,0.6426890921421813,0.7539277612115192,0.9884920830587489,0.4500227343380021,0.7556726109667422],[0.8388655655886612,0.9654633854019483,0.398082407423924,0.7059717357532838,0.43847544557808504,0.07045946575225748,0.44034410386684764,0.4095180083056167],[0.9120831420041495,0.9534148526051207,0.4575022945399021,0.4384717872377397,0.916123382584348,0.14378985691203727,0.4371103801212739,0.5348612981716783],[0.8927767293201567,0.422586441184148,0.4606164681191962,0.4396082653228448,0.820920925250918,0.37804660313495897,0.11012300048817347,0.12303523884053202]],"weight2":[[0.6352856913220923,0.6183761523872207,0.37279085978086046,0.8192460614238977],[0.3408695280492824,0.09997220310948585,0.08562676387279078,0.7926922769138558],[0.4065444129908533,0.953973214508417,0.9711809627401857,0.8283047492787412],[0.4665830563271647,0.938015380575365,0.3692694724741741,0.30862997550093185],[0.6632988118941925,0.7800820029745787,0.5994405704739152,0.8083900884759301],[0.4117424780881098,0.3046737808801463,0.5524930553089051,0.7662221215004259],[0.679479228633568,0.2362175386762122,0.5704629437587816,0.7327364785120778],[0.15414703587061362,0.15985902587723277,0.6849987063446856,0.44516980195192124]]})
 //    response = main(network) 
 //      /* Some variables were in "main" function ,
-//      users can call this function with inputValue , outputValueExpert and 
+//      users can call this function with inputValue , outputValueExpect and 
 //      action only after init.
 //      */
 //    main({"action":"speed","speed":1})
